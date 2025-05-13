b39a97ce388670f683d204a1481bd759ed7faf44
Jacqueline1603 <jacqueline16utt@gmail.com>
2025-05-13 15:33:45 -0600

    Ejercicio 1

---
+++ ejercicio01.py
@@ -0,0 +1,7 @@
+################################################################################
+# Escribir un programa que pregunte al usuario su nombre, y luego lo salude.
+################################################################################
+
+name = input ('Escribe tu nombre: \n')
+print ("Hola " + name)
+
