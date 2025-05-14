3325888e2f9411d1316a2dd1b7d39105ef160c38
Jacqueline1603 <jacqueline16utt@gmail.com>
2025-05-14 12:55:02 -0600

    Ejercicio 3

--- ejercicio03.py
+++ ejercicio03.py
@@ -3,12 +3,17 @@
 ################################################################################
 
 #Hipotenusa ** 2 = cateto 1 ** 2 + cateto 2 ** 2 
+#El operador doble asterisco ** permite elevar un numero
+## a una potencia n
+
+##Para operaciones matematicas avanzadas importamos la 
+##libreria math
+import math
 
 cateto1 = int(input ('Ingresa valor del Cateto 1: '))
 cateto2 = int(input ('Ingresa valor del Cateto 2: '))
 
 #sqrt es para la raiz cuadrada
-import math
 hipotenusa = math.sqrt (cateto1 ** 2 + cateto2 ** 2)
 
 print ('La hipotenusa del triangulo es: ' + str(hipotenusa))
