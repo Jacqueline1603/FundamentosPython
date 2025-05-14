70afc62080a7de4022e85b055a8a240540ec1e5b
Jacqueline1603 <jacqueline16utt@gmail.com>
2025-05-14 13:21:13 -0600

    ejercicio04

---
+++ ejercicio04.py
@@ -0,0 +1,19 @@
+################################################################################
+#Dados dos números, mostrar la suma, resta, división y multiplicación de ambos.
+################################################################################
+
+numero1 = int (input ('Inserta un numero: '))
+numero2 = int (input ('Inserta otro numero: '))
+
+suma = numero1 + numero2
+resta = numero1 - numero2
+
+print ('La suma es: ', suma)
+print ('La resta es: ', resta)
+
+print ('La multiplicacion es: ', numero1 * numero2)
+print ('La division es: ', numero1 / numero2)
+print ('EL residuo es: ', numero1 % numero2)   
+
+import math
+print ('El 1.7 redondeado es: ', round(1.7))
