6ab5144f8bbbc9e8cb4e2ecf79e4f171ee67181d
Jacqueline1603 <jacqueline16utt@gmail.com>
2025-05-22 10:09:49 -0600

    Ejercicio 1 de Condicionales

---
+++ Condicionales/ejercicio01.py
@@ -0,0 +1,10 @@
+#################################################################################
+#Algoritmo que pida dos números e indique si el primero es mayor que el segundo.
+#################################################################################
+
+numero1 = int (input('Ingresa un numero'))
+numero2 = int (input('Ingresa otro numero'))
+
+if numero1 > numero2:
+    print ('El primer numero es mayor')
+    
